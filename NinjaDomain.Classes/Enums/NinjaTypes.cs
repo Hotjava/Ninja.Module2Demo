738628d58456d3b4ea0cef7740d9e73a2f7570d0
jianbin.gao <jianbin.gao@ivanti.com>
2018-10-16 10:22:41 +0100

    Add project files.

---
+++ NinjaDomain.Classes/Enums/NinjaTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace NinjaDomain.Classes
+{
+    public enum NinjaType
+    {
+        Shinobi = 1,
+        Kunoichi = 2
+
+    }
+
+
+    public enum EquipmentType
+    {
+        Tool = 1,
+        Weapon = 2,
+        Outwear = 3
+    }
+}
